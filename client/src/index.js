a6f7e09be1aa1354922a04cbbd2e937742742eb8
monaecurbeam98 <curbeammonae@gmail.com>
2024-05-30 15:14:42 -0400

    new

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
